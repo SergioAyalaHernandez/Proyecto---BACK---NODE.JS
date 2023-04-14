a623935ac0585839fb3e12338c56819a7358c2dd
Sergio Ayala <sergio.ayala@aldeamo.com>
2023-04-14 12:49:16 -0500

    a

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const gama_routes = require("./routes/Gama");
 const reservation_routes = require("./routes/Reservation");
 const message_routes = require("./routes/Message");
 const car_routes = require("./routes/Car");
-const port = 3002;
+const port = process.env.PORT ||3002;
 const bodyParser = require("body-parser");
 
 mongoose.Promise = global.Promise;
